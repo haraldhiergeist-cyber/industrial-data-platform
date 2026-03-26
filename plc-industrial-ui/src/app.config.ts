9a8608929805af3fb05e6b4eb3cc5d5f29cd9606
Hiergeist, Harald <Harald.Hiergeist@sar.biz>
2026-03-26 11:03:24 +0100

    environment for base path added

--- plc-industrial-ui/src/app.config.ts
+++ plc-industrial-ui/src/app.config.ts
@@ -1,15 +1,26 @@
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection, importProvidersFrom  } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation, withInMemoryScrolling } from '@angular/router';
 import Aura from '@primeuix/themes/aura';
 import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
+
+import { ApiModule } from './app/api/api.module';
+import { Configuration } from './app/api/configuration';
+import { environment } from './environments/environment';
+
+export function apiConfigFactory(): Configuration {
+    return new Configuration({
+        basePath: environment.apiBasePath
+    });
+}
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(appRoutes, withInMemoryScrolling({ anchorScrolling: 'enabled', scrollPositionRestoration: 'enabled' }), withEnabledBlockingInitialNavigation()),
         provideHttpClient(withFetch()),
         provideZonelessChangeDetection(),
-        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } })
+        providePrimeNG({ theme: { preset: Aura, options: { darkModeSelector: '.app-dark' } } }),
+        importProvidersFrom(ApiModule.forRoot(apiConfigFactory))
     ]
 };
