84d54625f28cb943e78e41824ce0db00dd1a2490
harald <harald.hiergeist@gmail.com>
2026-03-27 10:21:48 +0100

    https statt http in cors for plc-query.cluster.lab

--- plc-query-service/src/main/java/com/plc/query/config/CorsConfig.java
+++ plc-query-service/src/main/java/com/plc/query/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200")
-                        .allowedOrigins("http://plc-query.cluster.lab")
+                        .allowedOrigins("https://plc-query.cluster.lab")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
